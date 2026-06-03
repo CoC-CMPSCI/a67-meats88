f68c1406e73b6b9a4545f0bfc19370be4c6c9a28
meats88 <pcx883mi@gmail.com>
2026-06-02 20:17:36 -0700

    finishsshs

--- main.hpp
+++ main.hpp
@@ -5,5 +5,10 @@ int hanoi(int n, char from, char to, char aux);
 
 int hanoi(int n, char from, char to, char aux)
 {
-    // TODO
+static int count = 0;
+    count++;
+    if (n == 1) return count;
+    hanoi(n - 1, from, aux, to);
+    hanoi(n - 1, aux, to, from);
+    return count;
 }
